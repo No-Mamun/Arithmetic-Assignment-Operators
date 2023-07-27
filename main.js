345ddd471417331bd5d17f9f5a68b1c85e206bff
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2023-07-27 14:59:42 +0600

    Add files via upload
    Arithmetic & Assignment Operators

---
+++ main.js
@@ -0,0 +1,102 @@
+// Arithmetic & Assignment Operators
+
+// syntax
+
+// Node:1
+
+// let x,y,z; declare a variable
+
+// x = 10;Assign a value
+
+// y = 4;Assign a value
+
+// z = x + y;calcualte a value
+
+// console.log(z);
+
+// Node:2
+
+/*
+x = 10;
+y = 4;
+z = x + y;
+
+console.log(z);
+
+*/
+
+// Arithmetic Operators
+
+/*
++ Addition
+- Subtraction
+* Multiplication
+/ Division (float)
+% Modulus / Remainder of division operation
+** Exponentiation or Square root calculation(ES6)
+++ Increment operator to increment the current number by one and return it as output after each execution. It
+also can be used for string concatenation in JavaScript. For example "Hello" ++ "World".
+
+*/
+
+/*
+Node:3
+x = 4;
+y = 2; // Assigning values
+sum = x % y; // Multiply two numbers using multiplication symbol (*). Result is stored into sum
+
+console.log(sum);
+
+*/
+
+/*
+Node:4
+let x = 10;
+++x
+++x
+++x
+console.log(x);
+
+*/
+
+/*
+Node:5
+let x = 10;
+x++
+x++
+x++
+
+console.log(x);
+
+*/
+
+/*
+Node:5
+let x = 10;
+--x
+--x
+--x
+
+console.log(x);
+
+*/
+
+/*
+Node:7
+let x = 10;
+x--
+x--
+x--
+
+console.log(x);
+*/
+
+/*
+Assignment Operators
+let x = 10;
+let y = 4;
+
+x = x / y;
+
+console.log(x);
+*/
